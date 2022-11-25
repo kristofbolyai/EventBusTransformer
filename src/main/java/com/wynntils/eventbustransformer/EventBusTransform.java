62f1200ba6582ce0458bfcf481f19889eb63de35
kristofbolyai <49001742+kristofbolyai@users.noreply.github.com>
2022-11-25 22:51:49 +0100

    fix: Don't transform classes that are not from wynntils

--- src/main/java/com/wynntils/eventbustransformer/EventBusTransform.java
+++ src/main/java/com/wynntils/eventbustransformer/EventBusTransform.java
@@ -23,8 +23,7 @@ public class EventBusTransform implements ClassEditTransformer {
         }
 
         Type type = Type.getObjectType(node.name);
-        LOGGER.debug("Trying to transform class " + type.getClassName());
-        if (engine.handlesClass(type)) {
+        if (engine.handlesClass(type) && type.getClassName().startsWith("com.wynntils")) {
             LOGGER.info("Transforming class " + type.getClassName());
             dev.architectury.transformer.shadowed.impl.org.objectweb.asm.ClassWriter architecturyClassWriter = new dev.architectury.transformer.shadowed.impl.org.objectweb.asm.ClassWriter(0);
             node.accept(architecturyClassWriter);
